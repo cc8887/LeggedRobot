c587b5abe75d00b4b4f06c9ee26dc579af264757
cc8887 <qq727626166@Gmail.com>
2019-05-10 15:02:19 +0800

    单腿动力学加入机器人本体速度，输入参数有改动

--- legDy.m
+++ legDy.m
@@ -1,5 +1,5 @@
-%传入robot 是机器人工具箱中
-function [wolPoint,wolV,F_tol,M_tol] = legDy(robot,pos,w,a,gravity)
+%传入robot 是机器人工具箱中的机器函数
+function [wolPoint,wolV,F_tol,M_tol,alpha] = legDy(robot,pos,w,a,gravity,vWater)
 
     %水密度
     pho = 1000;
@@ -20,7 +20,7 @@ function [wolPoint,wolV,F_tol,M_tol] = legDy(robot,pos,w,a,gravity)
     %用来存储每个单元的速度的和
     U = 0;
     %水流流速
-    vWater = [0;0;0];
+   % vWater = [0;0;0];
     %vWater = [0;0*sin(t);0*cos(t)];
     %用来存储每个单元产生的力矩
     M_tol = 0;
@@ -38,14 +38,16 @@ function [wolPoint,wolV,F_tol,M_tol] = legDy(robot,pos,w,a,gravity)
     for k = 0:dx:0.12
     %计算每个点在本地坐标系中的值
         %翼的方向在末关节坐标系中是沿着x方向的
+        %y轴垂直于翼的表面
         locPoint_x = k + bais;
-       
+        
         locV = endVel(1:3);
         locPoint = [locPoint_x;0;0];
         wolPoint = SEList(4)*locPoint;
         %到了这一步速度时以本地坐标系表示的
+        %locV = [dot(locV,SEList(4)*xi-endPoint);dot(locV,SEList(4)*yi-endPoint);dot(locV,SEList(4)*zi-endPoint)];
         locV = [0;dot(locV,SEList(4)*yi-endPoint);dot(locV,SEList(4)*zi-endPoint)];
-
+        %locV = [0;dot(locV,SEList(4)*yi);dot(locV,SEList(4)*zi)];
         locV_tamp = [w(4)*locPoint_x*yi;1];
         
       % T1 = trotz(pos(4)/pi*180);
@@ -58,18 +60,24 @@ function [wolPoint,wolV,F_tol,M_tol] = legDy(robot,pos,w,a,gravity)
         %求得攻角alpha
         %cosA = dot(locV_tamp,zi)/norm(U);
         %alpha = acos(cosA)
-        alpha = atan(locV_tamp(2)/locV_tamp(3));
-        alpha = abs(alpha);
+        alpha = atan(locV_tamp(3)/locV_tamp(2));
+        %alpha = atan(locV_tamp(3)/abs(locV_tamp(2)));
+        
+        
+        %！！！！！！！！！！！攻角该如何就是那这部分在好好查一查！！！！！！！！！！！！！！
+        % alpha = abs(alpha);
         
         %wolPoint = SEList(4)*locPoint;
         %relw = w(4)*zi;
         %vtamp = cross(locPoint,relw') + v(1:3) + cross(wolPoint',v(4:6));
         %U = (dot(vtamp',SEList(4)*yi))^2+(dot(vtamp',SEList(4)*zi))^2;
         %计算两个方向的合力
-        %计算升力并向量化，这里进行了简化，假设我们的升力系数为1
+        %计算升力并向量化
         L = 0.5.*pho*U^2.*zi*sin(2*alpha)*0.08*dx*CL(alpha);
+%          L = 0.5.*pho*U^2.*zi*sin(2*alpha)*0.08*dx;
         %计算阻力
         F = 0.5*pho*yi*U^2*cos(1-cos(2*alpha))*0.08*dx*CD(alpha);
+%         F = 0.5*pho*yi*U^2*cos(1-cos(2*alpha))*0.08*dx;
         %附加质量力
         F_m = pi*pho*locV_tamp*0.08*0.08/4*dx;
         %计算力在大地坐标系下的表示
